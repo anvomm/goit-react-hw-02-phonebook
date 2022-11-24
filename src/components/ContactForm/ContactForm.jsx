f416d5b5d8235127aa60cdbe8008227721ea8316
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-25 00:13:16 +0200

    inputHandler added

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,6 +5,10 @@ export class ContactForm extends Component {
     name: '',
   };
 
+  inputHandler = e => {
+    this.setState({ name: e.target.value });
+  };
+
   render() {
     return (
       <form>
@@ -13,8 +17,10 @@ export class ContactForm extends Component {
           <input
             type="text"
             name="name"
+            value={this.state.name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            onChange={this.inputHandler}
             required
           />
         </label>
