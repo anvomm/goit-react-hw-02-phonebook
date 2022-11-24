c562c89a19ba3e9629434fa01fff5eb6c47c6eec
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-24 23:40:39 +0200

    Form component created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm></ContactForm>
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+  };
+
+  render() {
+    return (
+      <form>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <button>Add contact</button>
+      </form>
+    );
+  }
+}
